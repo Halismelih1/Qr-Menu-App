9a814d13b9dd4968911d96a3bde0f8c660063f7a
Halismelih1 <hhlsabkk@icloud.com>
2023-12-11 02:16:53 +0300

    css fixed

--- src/Components/Welcome.jsx
+++ src/Components/Welcome.jsx
@@ -4,35 +4,39 @@ import { Link } from 'react-router-dom';
 
 const Welcome = () => {
   return (
-    <div className="bg-gray-400 text-white min-h-screen flex flex-col items-center justify-center overflow-hidden relative">
-      <Link to="/login" className="text-white py-2 px-4 cursor-pointer absolute top-0 right-0 ">
-        isAdmin ?
-      </Link>
-      <div className="max-w-xl p-8 bg-gray-500 rounded-md shadow-lg text-center relative">
-        <h1 className="text-5xl font-extrabold mb-4">Hoş Geldiniz!</h1>
-        <p className="text-gray-300 mb-8">Lezzetin Keyfini Çıkarın. Restoranımıza özel lezzetleri keşfedin.</p>
-
-        <Link
-          to="/menu"
-          className="bg-blue-500 text-white py-3 px-6 rounded-full mb-6 inline-block hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300"
-        >
-          Menüyü Görüntüle
+    <div className="min-h-screen bg-gradient-to-r from-gray-600 to-white flex flex-col items-center bg-no-repeat" >
+      {/* Logo */}
+      <div className="mt-32 mb-2">
+      <Link to="/login">
+          <img src="assets/dgrmn.png" alt="Logo" className="h-16 cursor-pointer" />
         </Link>
+        </div>
 
-        <div className="flex justify-center space-x-4">
-          <a href="#" className="text-gray-300 hover:text-blue-500">
-            <FaInstagram size={32} />
-          </a>
-          <a href="#" className="text-gray-300 hover:text-blue-500">
-            <FaTwitter size={32} />
-          </a>
-          <a href="#" className="text-gray-300 hover:text-blue-500">
-            <FaFacebook size={32} />
-          </a>
+      {/* Üst Div (Görsel ve Hoşgeldiniz Metni) */}
+      <div className="text-white text-center p-4 md:p-8">
+        <h1 className="text-2xl md:text-6xl font-extrabold mb-2 md:mb-4 m-16">Hoşgeldiniz!</h1>
+        <p className="text-gray-900">Tadını çıkarın, özel lezzetlerimizi keşfedin.</p>
+      </div>
+
+      {/* Alt Div (Menü Git Butonu ve Sosyal Medya Linkleri) */}
+      <div className="flex flex-col items-center space-y-4 p-4 md:p-8 ">
+      <Link to="/menu">
+      <button className="bg-white text-black px-4 md:px-6 py-2 md:py-3 rounded-full hover:bg-yellow-500 hover:text-white focus:outline-none focus:ring focus:border-blue-300">
+  Menüye Git
+</button>
+        </Link>
+        <div className="flex space-x-4">
+          <a href="#" className="text-gray-300 hover:bg-yellow-500 hover:text-white"><FaInstagram size={24} /></a>
+          <a href="#" className="text-gray-300 hover:bg-yellow-500 hover:text-white"><FaTwitter size={24} /></a>
+          <a href="#" className="text-gray-300 hover:bg-yellow-500 hover:text-white"><FaFacebook size={24} /></a>
         </div>
       </div>
     </div>
   );
 };
-
 export default Welcome;
+
+
+
+
+
