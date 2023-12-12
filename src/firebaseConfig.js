859bcbb1173caf68d0420accc40a4ae055409a45
Halismelih1 <hhlsabkk@icloud.com>
2023-12-13 01:26:35 +0300

    upd ignore

---
+++ src/firebaseConfig.js
@@ -0,0 +1,26 @@
+import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCTGDGkxEt7LY1PPmZp9oLO72t0OeKKp5g",
+    authDomain: "qr-menu-ep.firebaseapp.com",
+    projectId: "qr-menu-ep",
+    storageBucket: "qr-menu-ep.appspot.com",
+    messagingSenderId: "320241078228",
+    appId: "1:320241078228:web:446b7baa470a4e80c15041",
+    measurementId: "G-TCZJLJ7CCF"
+  };
+
+   const app = initializeApp(firebaseConfig);
+   const storage = getStorage(app);
+   const auth = getAuth(app);
+   const db = getFirestore(app);
+
+
+
+
+   export { app, storage,auth,db };
